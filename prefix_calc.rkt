8b23381d734eba3be8f455c817255fbb0ea231c1
Lane Meeks <147874358+lanemeeks@users.noreply.github.com>
2026-03-04 15:55:47 -0600

    devlog - session end

--- prefix_calc.rkt
+++ prefix_calc.rkt
@@ -1,16 +1,33 @@
 #lang racket
 ;tail-recursive helper function that given the prefix expression
 ;as a string it returns the result as a number
-(define (eval-exp expression-str)
+(define (eval-exp expression-str history)
   (define expression (with-input-from-string expression-str read));parsing string like input makes into data
 
+
+  ;defining another helper method to interpret use of $n
+  (define (int-hist s)
+    (cond
+      ((symbol? s)
+       (let* ((str (symbol->string s)))
+         (if (and (> (string-length str) 1)
+                  (char=? (string-ref str 0) #\$));establishing if the preceeding symbol was $
+             (let* ((id (string->number (substring str 1)))
+                    (rev (reverse history)))
+               (if (and id (<= 1 id) (<= id (length rev)))
+                   (list-ref rev (- id 1))
+                   (error "Error: Invalid history reference")))
+             s)))
+      (else s)))
+  
   ;helper function to remove nested lists within expression
   (define (remove-nests l)
     (cond
       ((number? l) (list l));returning the number as a list
+      ((symbol? l) (list (int-hist l)));dealing with history reference case
       ((list? l) (cons (car l)
                        (apply append (map remove-nests (cdr l)))));recursively remove lists inside the main one
-      (else (error "something is wrong"))))
+      (else (error "Error: Input contains invalid content"))))
   (define tokens (reverse (remove-nests expression)));remove nested statements and put them back without parenthesis and then reverse it
 
   (define (apply-operator op a b);cute lil method to avoid using eval
@@ -20,8 +37,9 @@
       ((eq? op '*) (* a b))
       ((eq? op '/)
        (if (zero? b)
-           (error "Division by zero")
-           (/ a b)))))
+           (error "Error: Division by zero")
+           (/ a b)))
+      (else (error "Error: Bad operator"))))
 
 
 
@@ -31,27 +49,48 @@
       ((null? remaining)
        (if (= (length stack) 1)
               (car stack);'popping' when there's one number left
-              (error "invalid input"))); if there nothing in the stack something went wrong
+              (error "Error: Input was invalid format"))); if there nothing in the stack something went wrong
        ((number? (car remaining));if it's a number
         (eval-loop (cdr remaining)
                    (cons (car remaining) stack)));call eval-loop again
        (else;token is operator case
-        (let* ((op (car remaining))
+        (if (< (length stack) 2)
+            (error "Error: too few operands")
+            (let* ((op (car remaining))
                (a (car stack))
                (b (cadr stack));get things from the stack
                (rest (cddr stack))
                (result (apply-operator op a b)))
           (eval-loop (cdr remaining)
-                     (cons result rest)))))));tail-recursion call it again on the rest of the things
+                     (cons result rest))))))));tail-recursion call it again on the rest of the things
 
 (define (user-input)
-  (let input-loop ()
+  (let input-loop ((history '()))
     (display "Enter prefix expression or \"quit\" to exit: ")
     (flush-output);makes sure the prompt is displayed
+
     (define input (read-line))
-    (if (or (eof-object? input)
-            (string=? input "quit"))
-        (displayln "Exiting Program")
-        (let ((result (eval-exp input)))
-          (displayln (string-append "Result: " (number->string result)))
-          (input-loop)))))
+    
+    (cond
+      ((or (eof-object? input)
+           (string=? input "quit"));user wants to quit the program
+       (displayln "Ending Program"))
+      ((or (string=? input "")
+          (regexp-match? #px"^\\s*$" input));checking if input is empty or just white spaces
+       (error "Error: Input is empty")
+       (input-loop history))
+      (else
+       (with-handlers
+           ((exn:fail?;try-catch statement checking for run time errors
+             (lambda (e)
+               (error "Error: runtime or syntax error caught by program")
+               (input-loop history))))
+         (let* ((result (eval-exp input history))
+                (new-history (cons result history));adding the result to the history list
+                (id (length new-history))
+                (float-val (real->double-flonum result)))
+           (display id)
+           (display ": ")
+           (display float-val)
+           (newline)
+           (input-loop new-history)))))))
